46f47f9b2df0aec2d042b1118d2f4e199c58b395
heavy <linruisheng@seeklane.com>
2021-04-07 15:55:08 +0800

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# http server for python
+
+python简单http服务器
+
+## 技术栈
+
+- [python的http服务器](https://bottlepy.org/docs/dev/)
+- [apidoc](https://apidocjs.com/#getting-started)
+
+## 启动服务器
+
+    python main.py
+
+* api文档 http://localhost:8080/static/index.html
+
+## 编译api文档
+
+    npm instal
+    npm run doc
+
---
+++ main.py
@@ -0,0 +1,36 @@
+from bottle import get, post, request, run, static_file, redirect
+import json
+
+"""
+@apiDefine jsonRequest
+@apiHeader {String} Content-Type=application/json
+"""
+
+## 首页
+@get('/')
+def index():
+    redirect('/static/index.html')
+
+## 静态文件
+@get('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static_file')
+
+
+"""
+@apiDescription 这是测试例子的描述
+@api {post} /hello/:name 测试例子
+@apiName name
+@apiGroup 测试
+@apiUse jsonRequest
+
+@apiParam {String} [lastName] your lastName
+"""
+@post('/hello')
+@post('/hello/<name>')
+def hello(name = "tom"):
+    # 路由参数name（可选）， post提交的参数在request.json
+    # 返回用json.dumps
+    return json.dumps({ 'name': name, 'request-Parameters': request.json })
+
+run(host='localhost', port=8080)
